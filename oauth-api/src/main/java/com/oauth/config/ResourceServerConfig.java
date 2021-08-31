fbc38e5f75f60101a221c863b1d0c22e055ed92f
zeeeeeno <2zeeno@naver.com>
2021-08-31 18:06:40 +0900

    oauth2.0 서버 구성

--- oauth-api/src/main/java/com/oauth/config/ResourceServerConfig.java
+++ oauth-api/src/main/java/com/oauth/config/ResourceServerConfig.java
@@ -1,10 +1,45 @@
 package com.oauth.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
 /**
  * Description :
  *
  * @author leejinho
  * @version 1.0
  */
-public class ResourceServerConfig {
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Value("${security.oauth2.jwt.signkey}")
+    private String signKey;
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setSigningKey(signKey);
+        return jwtAccessTokenConverter;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().disable();
+        http.authorizeRequests()
+                .antMatchers("/api/userinfo").access("#oauth2.hasScope('read')")
+                .anyRequest().authenticated();
+    }
 }
--- oauth-server/src/main/java/com/oauth/config/Oauth2AuthorizationConfig.java
+++ oauth-server/src/main/java/com/oauth/config/Oauth2AuthorizationConfig.java
@@ -1,6 +1,11 @@
 package com.oauth.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 /**
  * Description :
@@ -9,5 +14,19 @@ import org.springframework.context.annotation.Configuration;
  * @version 1.0
  */
 @Configuration
-public class Oauth2AuthorizationConfig {
+public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Value("${security.oauth2.jwt.signkey}")
+    private String signKey;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        super.configure(endpoints);
+        endpoints.accessTokenConverter(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        return new JwtAccessTokenConverter();
+    }
 }
